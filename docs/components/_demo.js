b088945a630fd78aa55f894c19aa1a077c6b374d
yangxin <471748942@qq.com>
2017-04-20 17:01:39 +0800

    完善demo组件 修改app flex 使用demo组件

--- docs/components/_demo.js
+++ docs/components/_demo.js
@@ -3,21 +3,45 @@ const template = `
     <div class="docs-panel-content">
         <slot></slot>
     </div>
-    <div class="docs-code">
-        <pre><code>{{code}}</code></pre>
+    <au-button-group>
+        <au-button type="primary"  v-show="!show" @click="toggle">展开代码</au-button>
+        <au-button type="primary"  v-show="show" @click="toggle">收起代码</au-button>
+    </au-button-group>
+    <div class="docs-code" v-show="show">
+        <pre><code>{{sourceCode}}</code></pre>
     </div>
 </div>
 `;
+
+function htmlEncode(str){
+  var s = "";
+  if (str.length == 0) return "";
+  s = str.replace(/&/g, "&gt;");
+  s = s.replace(/</g, "&lt;");
+  s = s.replace(/>/g, "&gt;");
+  s = s.replace(/ /g, "&nbsp;");
+  s = s.replace(/\'/g, "&#39;");
+  s = s.replace(/\"/g, "&quot;");
+  s = s.replace(/\n/g, "<br>");
+  return s;
+}
+
+function convert(html) {
+    html = html.trim();
+    html = html_beautify(html, {
+      indent_size: 2
+    });
+    html = html.replace(/=""/g, '');
+    return htmlEncode(html);
+}
 
 const Codes = {};
 
 const demo = Vue.extend({
     template: template,
-    props: {
-        codeId: '',
-    },
+    props: ['codeId', 'code'],
     data(){
-        return {code: ''}
+        return {show: false}
     },
     beforeCreate(){
         var eles = document.querySelectorAll('demo');
@@ -27,11 +51,29 @@ const demo = Vue.extend({
             }
         });
     },
-    beforeMount(){
-        this.code = Codes[this.codeId];
+    mounted(){
+        this.updateCode();
     },
-    mounted() {
-        hljs.highlightBlock(this.$el.querySelector('code'))
+    methods: {
+        toggle(){
+            this.show = !this.show;
+        },
+        updateCode(){
+            let newCode = document.createElement('code');
+            let codeEle = this.$el.querySelector('code');
+            newCode.innerHTML = convert(this.sourceCode);
+            codeEle.replaceWith(newCode);
+            hljs.highlightBlock(newCode);
+        }
+    },
+    computed: {
+        sourceCode(){return this.code || Codes[this.codeId];}
+    },
+    watch: {
+        code(val){
+            Vue.nextTick(_=>this.updateCode());
+        }
+
     }
 })
 
