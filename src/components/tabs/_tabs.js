f6f5ca5865d9df5fd456ed8e59acdcf76206c9aa
konakona <lovekonakona@gmail.com>
2017-04-26 15:47:33 +0800

    Update for tab

--- src/components/tabs/_tabs.js
+++ src/components/tabs/_tabs.js
@@ -1,104 +1,84 @@
+import dispatch from '../../mixins/_dispatch'
 import AuTabPanel from './_tab-panel.js'
 
 const AuTabs = Vue.extend({
   template: require('./_tabs.jade'),
+  mixins: [dispatch],
   props: {
     value: [String, Number]
   },
   data () {
     return {
-      tabs: [],
-      currentValue: '',
-      activeLineStyle: {}
+      panels: [],
+      fakeValue: ''
+    }
+  },
+  computed: {
+    model: {
+      get () {
+        return this.value || this.fakeValue
+      },
+      set (value) {
+        this.$emit('input', value)
+        this.fakeValue = value
+      }
     }
   },
   updated () {
-    const tabs = this.getTabs()
+    this.calLineStyle()
+  },
+  created () {
+    this.$on('add-tab-panel', (panel) => {
+      this.initPanels()
+    })
 
-    if (this.isEqualTabs(this.tabs, tabs)) {
-      return
-    }
-
-    this.tabs = tabs
-    this.$nextTick(() => {
-      this.$forceUpdate()
-      this.$nextTick(() => {
-        this.setChildrenActive()
-        this.$nextTick(this.calLineStyle)
-      })
+    this.$on('remove-tab-panel', (panel) => {
+      const pos = this.panels.indexOf(panel)
+      if (pos > -1) {
+        this.panels.splice(pos, 1)
+      }
     })
   },
-  mounted () {
-    this.initTabs()
-
-    if (!this.value) {
-      if (this.tabs.length > 0) {
-        this.setValue(this.tabs[0].value)
-      }
-    } else {
-      this.currentValue = this.value
-      this.setChildrenActive()
-      this.$nextTick(this.calLineStyle)
-    }
-  },
   methods: {
-    isEqualTabs (tabs1, tabs2) {
-      return JSON.stringify(tabs1) === JSON.stringify(tabs2)
-    },
-    initTabs () {
-      this.tabs = this.getTabs()
-    },
-    getTabs () {
-      const tabs = []
-      const slot = this.$slots.default
-
-      if (slot) {
-        slot.forEach((item) => {
-          if (item.componentOptions && item.componentOptions.Ctor == AuTabPanel) {
-            const props = item.componentOptions.propsData
-            tabs.push(props)
-          }
-        })
+    initPanels () {
+      this.panels = this.$children.filter((child) => {
+        return child instanceof AuTabPanel
+      })
+      this.setChildrenActive()
+      if (!this.model) {
+        if (this.panels.length > 0) {
+          this.model = this.panels[0].value
+        }
       }
-      return tabs
     },
     clickTab (tab) {
       if (!tab.disabled) {
+        this.model = tab.value
         this.$emit('tab-click', tab)
-        this.setValue(tab.value)
       }
     },
-    setValue (value) {
-
-      this.$emit('input', value)
-      this.currentValue = value
-      this.setChildrenActive()
-      this.$nextTick(this.calLineStyle)
-    },
     setChildrenActive () {
-      this.$children.some((component) => {
-        if (!(component instanceof AuTabPanel)) {
-          return
-        }
-        if (component.value === this.currentValue) {
-          component.active = true
-        } else {
-          component.active = false
-        }
+      this.panels.forEach((panel) => {
+        panel.active = panel.value === this.model
       })
     },
     calLineStyle () {
       const heading = this.$el.querySelector('.au-tabs-heading')
-      const items = heading.querySelectorAll('.au-tab-item.active')
+      const active = heading.querySelector('.au-tab-item.active')
+      const activeLine = this.$refs.activeLine
 
-      if (items && items.length > 0) {
-        this.activeLineStyle = {
-          width: items[0].offsetWidth + 'px',
-          left: items[0].offsetLeft + 'px'
-        }
+      if (active != null) {
+        activeLine.style.width = active.offsetWidth + 'px'
+        activeLine.style.left = active.offsetLeft + 'px'
       } else {
-        this.activeLineStyle = {}
+        activeLine.style.width = ''
+        activeLine.style.left = ''
       }
+    }
+  },
+  watch: {
+    model () {
+      this.setChildrenActive()
     }
   }
 })
